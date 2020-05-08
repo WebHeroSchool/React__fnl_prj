c432435c9955a785160bae53f4f94aa6ead32208
AlinA414 <62790604+AlinA414@users.noreply.github.com>
2020-05-08 19:03:25 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import Footer from '..Footer/Footer';
 import ItemList from '../ItemList/ItemList';
 
+const props = {
+   firstItem:'Пройти все уроки блока react.',
+   secondItem:'Сдать тест по react.',
+   thirdItem:'Сделать финальный проект.',
+};
+   
 const App = () => (
-   <body>
-       <h1>Todo list</h1>
-       <ItemList />
-       <Footer />
-   </body>
+   <div>
+       <h1>Список дел:</h1>
+       <ItemList props = {props}/>
+       <Footer count = {3}/>
+   </div>
  );
  
  export default App;
