9f6343f12bf5d8f285985f040d115a5e7761efe9
AlinA414 <62790604+AlinA414@users.noreply.github.com>
2020-05-10 09:42:48 +0200

    Update App.js
    lesson 21

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,18 +2,26 @@ import React from 'react';
 import Footer from '../Footer/Footer';
 import ItemList from '../ItemList/ItemList';
 
-const props = {
-   firstItem:'Пройти все уроки блока react.',
-   secondItem:'Сдать тест по react.',
-   thirdItem:'Сделать финальный проект.'
-};
+const props = () => {
+   const props = [
+      {
+         value: 'Пройти все уроки блока react.',
+      };
+      {
+         value: 'Сдать тест по react.',
+      };
+      {
+         value: 'Сделать финальный проект.'
+      }
+];
    
-const App = () => (
-   <div>
+   return (
+      <div>
        <h1>Список дел:</h1>
        <ItemList props = {props}/>
        <Footer count = {3}/>
    </div>
- );
+  );      
+};
  
- export default App;
+export default App;
