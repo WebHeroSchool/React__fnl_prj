bfe026be5b717491cf719141be8380039a481413
AlinA414 <62790604+AlinA414@users.noreply.github.com>
2020-05-10 17:21:32 +0200

    Update InputItem.js

---
+++ src/components/InputItem/InputItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TexField from '@material-ui/core/exField';
+
+const InputItem = () => (<p>
+    <TexField
+       id="standard-dense"
+       label="Dense"
+       margin="dense"
+    />
+</p>);
+
+export default InputItem;
