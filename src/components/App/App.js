9d7e95a363450ddafa9444d6a8411f27410ae8b6
AlinA414 <62790604+AlinA414@users.noreply.github.com>
2020-05-10 10:36:09 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,18 +3,27 @@ import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
 import ItemList from '../ItemList/ItemList';
 
-const props = {
-   firstItem:'Пройти все уроки блока react.',
-   secondItem:'Сдать тест по react.',
-   thirdItem:'Сделать финальный проект.'
-};
+const App = () => {
+   const props = [
+      {
+         value: 'Пройти все уроки блока react.',
+      };
+      {
+         value: 'Сдать тест по react.',
+      };
+      {
+         value: 'Сделать финальный проект.'
+      }
+];
    
-const App = () => (
-   <div>
-       <h1>Список дел:</h1>
-       <ItemList props = {props}/>
+   return (
+      <div className="wrap">
+       <h1 className="wrap__title">Список дел:</h1>
+       <InputItem/>
+       <ItemList props = {props} className="wrap__list"/>
        <Footer count = {3}/>
    </div>
- );
+  );      
+};
  
- export default App;
+export default App;
