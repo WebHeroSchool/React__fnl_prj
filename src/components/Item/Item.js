1613e352a6dcd4531e6a1b3a3e97a0ffd92ca64c
Alina Omelchenko <omelchenkoalina96@gmail.com>
2020-05-10 09:32:02 +0200

    InputItem

---
+++ src/components/Item/Item.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Item = ({props}) => (<p>{props}</p>);
+
+export default Item;
---
+++ src/components/ItemList/ItemList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Item from '../Item/Item';
+
+const ItemList = ({props}) => (<ol>
+    <li><Item props = {props.firstItem}/></li>
+    <li><Item props = {props.secondItem}/></li>
+    <li><Item props = {props.thirdItem}/></li>
+</ol>);
+
+export default ItemList;
