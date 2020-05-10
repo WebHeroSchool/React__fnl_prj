f922ecac5512ab15c7584aa43a9ffe595c0083eb
AlinA414 <62790604+AlinA414@users.noreply.github.com>
2020-05-10 09:34:24 +0200

    Update App.js

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+import ItemList from '../ItemList/ItemList';
+
+const props = {
+   firstItem:'Пройти все уроки блока react.',
+   secondItem:'Сдать тест по react.',
+   thirdItem:'Сделать финальный проект.'
+};
+   
+const App = () => (
+   <div>
+       <h1>Список дел:</h1>
+       <ItemList props = {props}/>
+       <Footer count = {3}/>
+   </div>
+ );
+ 
+ export default App;
