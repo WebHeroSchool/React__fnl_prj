2f258d17e85ac00106d94bbef7fe74fd69ce7ddc
AlinA414 <62790604+AlinA414@users.noreply.github.com>
2020-05-11 09:10:44 +0200

    Update Item.js

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import classnames from 'classnames';
+import styles from './Item.module.css';
 
-const Item = ({props}) => (<p>{props}</p>);
+const Item = ({value, isDone, index}) => (<label htmlFor={`checkbox[${index}]`} className={
+    classnames({
+        [styles.item]: true,
+        [styles.done]: isDone
+    })
+}>
+    {value}
+</label>;
 
 export default Item;
