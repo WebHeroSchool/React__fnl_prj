59ef591881aa61b2457c673fd11b7988a4a0387e
AlinA414 <62790604+AlinA414@users.noreply.github.com>
2020-05-12 11:46:48 +0200

    Update Item.js

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import classnames from 'classnames';
+import styles from './Item.module.css';
+import PropTypes from 'prop-types';
 
-const Item = ({props}) => (<p>{props}</p>);
+const Item = ({value, isDone, index, onClickDone, id}) => (
+<label 
+    htmlFor={`checkbox[${index}]`} 
+    onClick={() => onClickDone(id)}
+    className={
+        classnames({
+            [styles.item]: true,
+            [styles.done]: isDone,
+    })}>
+    {value}
+</label>);
+
+Item.defaultProps ={
+    value: 'Задание',
+    isDone: false
+};
+
+Item.PropTypes = {
+    onClickDone: PropTypes.func.isRequired,
+    value: PropTypes.string.isRequired
+};
 
 export default Item;
