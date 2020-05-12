7fa885bbf1dd4252305526d34eaaee154eeec8d5
AlinA414 <62790604+AlinA414@users.noreply.github.com>
2020-05-12 14:57:54 +0200

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import styles from './Footer.module.css';
+import PropTypes from 'prop-types';
 
-const Footer = ({ count }) => (<p>Вот сколько дел еще осталось сделать: { count }</p>);
+const Footer = ({ count }) => (<p className={styles.text}>Вот сколько дел еще осталось сделать: { count }</p>);
+
+Footer.defaultProps ={
+    count: 4
+};
+
+Footer.propTypes ={
+    count: PropTypes.number.isRequired
+};
  
- export default Footer;
+export default Footer;
