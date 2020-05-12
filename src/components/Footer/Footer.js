0ca2c507cc4a699de4a853157c944b9bd7c4667f
AlinA414 <62790604+AlinA414@users.noreply.github.com>
2020-05-12 09:38:54 +0200

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import styles from './Footer.module.css';
 
-const Footer = ({ count }) => (<p>Вот сколько дел еще осталось сделать: { count }</p>);
+const Footer = ({ count }) => (<p className={styles.text}>Вот сколько дел еще осталось сделать: { count }</p>);
  
- export default Footer;
+export default Footer;
