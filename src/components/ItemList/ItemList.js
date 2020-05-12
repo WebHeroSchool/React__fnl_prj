50f292a0f34038e718bdaed7af8deb43db274699
AlinA414 <62790604+AlinA414@users.noreply.github.com>
2020-05-12 15:03:58 +0200

    Update ItemList.js

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import Item from '../Item/Item';
+import styles from './ItemList.module.css';
+import PropTypes from 'prop-types';
 
-const ItemList = ({props}) => (<ol>
-    <li><Item props = {props.firstItem}/></li>
-    <li><Item props = {props.secondItem}/></li>
-    <li><Item props = {props.thirdItem}/></li>
+const ItemList = ({props, onClickDone, onClickDelete}) => (<ol className={styles.list}>
+     {props.map((item, index) => (
+     <li className={styles.item} key={item.value} id={item.id}>
+          <input type="checkbox" className={styles.checkbox} id={`checkbox[${index}]`}/>
+          <Item 
+              value = {item.value} 
+              isDone={item.isDone} 
+              index={index} 
+              id={item.id}
+              onClickDone={onClickDone}
+          />
+          <button type="button" 
+                  onClick ={() => onClickDelete(item.id)}
+                  className={styles.button}>x</button>
+     </li>))}
 </ol>);
+
+ItemList.propTypes = {
+     onClickDelete: PropTypes.func.isRequired
+};
 
 export default ItemList;
